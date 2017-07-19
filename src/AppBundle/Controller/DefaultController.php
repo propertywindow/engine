fce9d59e11eb12405d7785d01a178967b7e7ae3c
geurtsmarc <geurtsmarc@hotmail.com>
2017-07-19 20:00:01 +0200

    add json response

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,7 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * Class DefaultController
@@ -13,14 +13,24 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="index")
+     *
+     * @return JsonResponse
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return new JsonResponse('no-access');
+    }
+
+
+    /**
+     * @Route("/blaat", name="blaat")
+     *
+     * @return JsonResponse
+     */
+    public function blaatAction()
+    {
+        return new JsonResponse('no-sdfsdf');
     }
 
 }
