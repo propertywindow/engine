1048bfd8097a5f2085733aefa0f7e6bb3bfa89d3
geurtsmarc <geurtsmarc@hotmail.com>
2017-07-21 23:56:04 +0200

    add primary key to property entity

--- src/PropertyBundle/Entity/Property.php
+++ src/PropertyBundle/Entity/Property.php
@@ -10,5 +10,23 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Property
 {
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
 
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 }
