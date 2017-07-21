588a388b6e2964b6bea570b54470395228bed269
geurtsmarc <geurtsmarc@hotmail.com>
2017-07-21 23:54:06 +0200

    add agent entities

---
+++ src/AuthenticationBundle/Entity/Agent.php
@@ -0,0 +1,530 @@
+<?php declare(strict_types=1);
+
+namespace AuthenticationBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Agent
+ *
+ * @ORM\Table(name="agent")
+ * @ORM\Entity(repositoryClass="AuthenticationBundle\Repository\AgentRepository")
+ */
+class Agent
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="groupId", type="integer")
+     */
+    private $groupId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="phone", type="string", length=255)
+     */
+    private $phone;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="fax", type="string", length=255)
+     */
+    private $fax;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="website", type="string", length=255)
+     */
+    private $website;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="logo", type="string", length=255)
+     */
+    private $logo;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address", type="string", length=255)
+     */
+    private $address;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="houseNumber", type="string", length=255)
+     */
+    private $houseNumber;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="postcode", type="string", length=255)
+     */
+    private $postcode;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="city", type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="country", type="string", length=255)
+     */
+    private $country;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="propertyLimit", type="integer")
+     */
+    private $propertyLimit;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="webprint", type="boolean")
+     */
+    private $webprint;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="espc", type="boolean")
+     */
+    private $espc;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="archived", type="boolean")
+     */
+    private $archived;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set groupId
+     *
+     * @param integer $groupId
+     *
+     * @return Agent
+     */
+    public function setGroupId($groupId)
+    {
+        $this->groupId = $groupId;
+
+        return $this;
+    }
+
+    /**
+     * Get groupId
+     *
+     * @return int
+     */
+    public function getGroupId()
+    {
+        return $this->groupId;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Agent
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set phone
+     *
+     * @param string $phone
+     *
+     * @return Agent
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    /**
+     * Get phone
+     *
+     * @return string
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * Set fax
+     *
+     * @param string $fax
+     *
+     * @return Agent
+     */
+    public function setFax($fax)
+    {
+        $this->fax = $fax;
+
+        return $this;
+    }
+
+    /**
+     * Get fax
+     *
+     * @return string
+     */
+    public function getFax()
+    {
+        return $this->fax;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Agent
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set website
+     *
+     * @param string $website
+     *
+     * @return Agent
+     */
+    public function setWebsite($website)
+    {
+        $this->website = $website;
+
+        return $this;
+    }
+
+    /**
+     * Get website
+     *
+     * @return string
+     */
+    public function getWebsite()
+    {
+        return $this->website;
+    }
+
+    /**
+     * Set logo
+     *
+     * @param string $logo
+     *
+     * @return Agent
+     */
+    public function setLogo($logo)
+    {
+        $this->logo = $logo;
+
+        return $this;
+    }
+
+    /**
+     * Get logo
+     *
+     * @return string
+     */
+    public function getLogo()
+    {
+        return $this->logo;
+    }
+
+    /**
+     * Set address
+     *
+     * @param string $address
+     *
+     * @return Agent
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    /**
+     * Get address
+     *
+     * @return string
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Set houseNumber
+     *
+     * @param string $houseNumber
+     *
+     * @return Agent
+     */
+    public function setHouseNumber($houseNumber)
+    {
+        $this->houseNumber = $houseNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get houseNumber
+     *
+     * @return string
+     */
+    public function getHouseNumber()
+    {
+        return $this->houseNumber;
+    }
+
+    /**
+     * Set postcode
+     *
+     * @param string $postcode
+     *
+     * @return Agent
+     */
+    public function setPostcode($postcode)
+    {
+        $this->postcode = $postcode;
+
+        return $this;
+    }
+
+    /**
+     * Get postcode
+     *
+     * @return string
+     */
+    public function getPostcode()
+    {
+        return $this->postcode;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     *
+     * @return Agent
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set country
+     *
+     * @param string $country
+     *
+     * @return Agent
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * Get country
+     *
+     * @return string
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * Set propertyLimit
+     *
+     * @param integer $propertyLimit
+     *
+     * @return Agent
+     */
+    public function setPropertyLimit($propertyLimit)
+    {
+        $this->propertyLimit = $propertyLimit;
+
+        return $this;
+    }
+
+    /**
+     * Get propertyLimit
+     *
+     * @return int
+     */
+    public function getPropertyLimit()
+    {
+        return $this->propertyLimit;
+    }
+
+    /**
+     * Set webprint
+     *
+     * @param boolean $webprint
+     *
+     * @return Agent
+     */
+    public function setWebprint($webprint)
+    {
+        $this->webprint = $webprint;
+
+        return $this;
+    }
+
+    /**
+     * Get webprint
+     *
+     * @return bool
+     */
+    public function getWebprint()
+    {
+        return $this->webprint;
+    }
+
+    /**
+     * Set espc
+     *
+     * @param boolean $espc
+     *
+     * @return Agent
+     */
+    public function setEspc($espc)
+    {
+        $this->espc = $espc;
+
+        return $this;
+    }
+
+    /**
+     * Get espc
+     *
+     * @return bool
+     */
+    public function getEspc()
+    {
+        return $this->espc;
+    }
+
+    /**
+     * Set archived
+     *
+     * @param boolean $archived
+     *
+     * @return Agent
+     */
+    public function setArchived($archived)
+    {
+        $this->archived = $archived;
+
+        return $this;
+    }
+
+    /**
+     * Get archived
+     *
+     * @return bool
+     */
+    public function getArchived()
+    {
+        return $this->archived;
+    }
+}
---
+++ src/AuthenticationBundle/Repository/AgentGroupRepository.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace AuthenticationBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * AgentGroupRepository
+ *
+ */
+class AgentGroupRepository extends EntityRepository
+{
+}
