9016f0d379ede7e48eb42716b633b7be88ae4ed4
geurtsmarc <geurtsmarc@hotmail.com>
2017-08-30 17:11:07 +0200

    add serviceGroupTemplate fixtures

---
+++ src/AuthenticationBundle/DataFixtures/ORM/LoadServiceGroupTemplateData.php
@@ -0,0 +1,199 @@
+<?php declare(strict_types=1);
+
+namespace AuthenticationBundle\DataFixtures\ORM;
+
+use AuthenticationBundle\Entity\ServiceGroupTemplate;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+
+/**
+ * Class LoadServiceGroupTemplateData
+ * @package AuthenticationBundle\DataFixtures\ORM
+ */
+class LoadServiceGroupTemplateData extends AbstractFixture implements OrderedFixtureInterface
+{
+    /**
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        // Admin
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_users'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_admin'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_agents'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_admin'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_services_manager'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_admin'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_inbox'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_admin'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_notifications'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_admin'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_invoices'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_admin'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_statistics'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_admin'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_settings'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_admin'));
+        $manager->persist($serviceGroupTemplate);
+
+        // Agent
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_users'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_agent'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_inbox'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_agent'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_notifications'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_agent'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_statistics'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_agent'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_settings'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_agent'));
+        $manager->persist($serviceGroupTemplate);
+
+        // Colleague
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_properties'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_colleague'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_appointments'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_colleague'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_viewings'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_colleague'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_interest'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_colleague'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_offers'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_colleague'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_surveys'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_colleague'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_contacts'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_colleague'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_email_alerts'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_colleague'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_inbox'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_colleague'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_notifications'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_colleague'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_statistics'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_colleague'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_settings'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_colleague'));
+        $manager->persist($serviceGroupTemplate);
+
+        // Client
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_properties'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_client'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_appointments'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_client'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_viewings'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_client'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_interest'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_client'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_offers'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_client'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_surveys'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_client'));
+        $manager->persist($serviceGroupTemplate);
+
+        $serviceGroupTemplate = new ServiceGroupTemplate();
+        $serviceGroupTemplate->setServiceGroup($this->getReference('service_group_statistics'));
+        $serviceGroupTemplate->setUserType($this->getReference('user_type_client'));
+        $manager->persist($serviceGroupTemplate);
+
+        $manager->flush();
+    }
+
+    /**
+     * @return integer
+     */
+    public function getOrder()
+    {
+        return 13;
+    }
+}
