d7489a05c9935d9f6e14924d06695f6c0448f678
geurtsmarc <geurtsmarc@hotmail.com>
2017-08-31 14:27:05 +0200

    add random user generator

--- src/AuthenticationBundle/DataFixtures/ORM/LoadUserData.php
+++ src/AuthenticationBundle/DataFixtures/ORM/LoadUserData.php
@@ -50,24 +50,40 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
         $this->addReference('user_2', $user);
         $manager->persist($user);
 
-        $user = new User();
-        $user->setUserType($this->getReference('user_type_client'));
-        $user->setAgent($this->getReference('agent_1'));
-        $user->setUsername('marc');
-        $user->setPassword(md5('marc'));
-        $user->setEmail('geurtsmarc@hotmail.com');
-        $user->setFirstName('Marc');
-        $user->setLastName('Geurts');
-        $user->setStreet('Graafsedijk');
-        $user->setHouseNumber('19');
-        $user->setPostcode('5437 NG');
-        $user->setCity('Beers');
-        $user->setCountry('NL');
-        $this->addReference('user_3', $user);
-        $manager->persist($user);
+
+        for ($i = 3; $i <= 10; $i++) {
+            $path     = file_get_contents('https://randomuser.me/api/?nat=gb');
+            $json     = json_decode($path, true);
+            $fakeUser = $json['results'][0];
+            $matches  = [];
+
+            if (preg_match('/(?P<number>\d+.?) (?P<street>[^\d]+)/', $fakeUser['location']['street'], $matches)) {
+                $houseNumber = $matches['number'];
+                $street      = $matches['street'];
+
+                $user = new User();
+                $user->setUserType($this->getReference('user_type_client'));
+                $user->setAgent($this->getReference('agent_1'));
+                $user->setUsername($fakeUser['login']['username']);
+                $user->setPassword($fakeUser['login']['md5']);
+                $user->setEmail($fakeUser['email']);
+                $user->setFirstName(ucfirst($fakeUser['name']['first']));
+                $user->setLastName(ucfirst($fakeUser['name']['last']));
+                $user->setStreet(ucwords($street));
+                $user->setHouseNumber($houseNumber);
+                $user->setPostcode($fakeUser['location']['postcode']);
+                $user->setCity(ucwords($fakeUser['location']['city']));
+                $user->setCountry('GB');
+                $user->setPhone($fakeUser['phone']);
+                $user->setAvatar($fakeUser['picture']['large']);
+                $this->addReference('user_'.$i, $user);
+                $manager->persist($user);
+            }
+        }
 
         $manager->flush();
     }
+
 
     /**
      * @return integer
