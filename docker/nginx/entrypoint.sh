a7b925a08b95075220134d932cd96dbc2ad5bdae
Marc Geurts <geurtsmarc@hotmail.com>
2017-12-19 16:44:39 +0100

    remove dockerfiles from repo

--- docker/nginx/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo 'CLEARING SYMFONY CACHE AND LOGS..'
-rm -rf /app/var/cache/*
-rm -rf /app/var/logs/*
-echo 'CACHE CLEARED!'
-
-exec $(which nginx) -c /etc/nginx/nginx.conf -g "daemon off;"
