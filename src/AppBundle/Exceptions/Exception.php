684fa9077c4bcae7ec37e118ae9c545e6d2218da
Marc Geurts <geurtsmarc@hotmail.com>
2018-02-12 10:25:49 +0100

    Test new exception

--- src/AppBundle/Exceptions/Exception.php
+++ src/AppBundle/Exceptions/Exception.php
@@ -3,10 +3,27 @@
 namespace AppBundle\Exceptions;
 
 use Exception as BaseException;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Throwable;
 
 /**
  * @package AppBundle\Exceptions
  */
 class Exception extends BaseException
 {
+    /**
+     * @param string         $message
+     * @param int            $code
+     * @param Throwable|null $previous
+     */
+    public function __construct(string $message = "", int $code = 400, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+
+        return new JsonResponse(
+            $message,
+            $code
+        );
+
+    }
 }
