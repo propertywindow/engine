a5d0bc1b26f01054974c695c03fed897d301a38c
Marc Geurts <geurtsmarc@hotmail.com>
2018-02-12 11:01:48 +0100

    Revert

--- src/AppBundle/Exceptions/Exception.php
+++ src/AppBundle/Exceptions/Exception.php
@@ -1,30 +1,13 @@
-<?php declare(strict_types = 1);
+<?php
+declare(strict_types = 1);
 
 namespace AppBundle\Exceptions;
 
 use Exception as BaseException;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Throwable;
 
 /**
  * Class Exception
  */
 class Exception extends BaseException
 {
-    /**
-     * @param string         $message
-     * @param int            $code
-     * @param Throwable|null $previous
-     */
-    public function __construct(string $message = "", int $code = 400, Throwable $previous = null)
-    {
-        //        parent::__construct($message, $code, $previous);
-
-        return [
-            'error' => [
-                'code'    => $code,
-                'message' => $message ? : 'API Exception',
-            ],
-        ];
-    }
 }
